47204c549e5b47dd119c77ce1e426088aa4a1eab
Florian Barth <florianbarth@gmx.de>
2017-04-19 13:47:29 +0200

    Use union-find data structure for counting components

--- src/ae1/alg.rs
+++ src/ae1/alg.rs
@@ -3,45 +3,70 @@ use super::{Edge, Graph, NodeId};
 use std::collections::{BTreeSet, VecDeque};
 use std::time::Instant;
 
-type Component = BTreeSet<NodeId>;
 impl<E: Edge> Graph<E> {
     pub fn count_components(&self) -> usize {
         let start = Instant::now();
-        let mut visited = vec![false; self.node_info.len()];
-        let mut components = Vec::<Component>::new();
-        for id in 0..visited.len() {
-            if !visited[id] {
-                let mut comp = self.dfs(id, &mut visited);
-                for component in &mut components {
-                    if !component.is_disjoint(&comp) {
-                        component.append(&mut comp);
-                        break;
-                    }
-                }
-                if !comp.is_empty() {
-                    components.push(comp);
-                }
-            } else {
-                continue;
+        let mut union = UnionFind::new(self.node_info.len());
+        for id in 0..self.node_info.len() {
+            if id == union.find(id) {
+                self.dfs(id, &mut union);
             }
         }
-
+        let count = union.count();
         let end = Instant::now();
         println!("Counting Components took {:?}", end.duration_since(start));
-        components.len()
+        count
     }
 
-    fn dfs(&self, start: NodeId, visited: &mut Vec<bool>) -> Component {
-        let mut result = Component::new();
-        let mut queue = VecDeque::<NodeId>::new();
-        queue.push_front(start);
-        while let Some(n) = queue.pop_front() {
-            result.insert(n);
-            if !visited[n] {
-                visited[n] = true;
+    fn dfs(&self, start: NodeId, union: &mut UnionFind) {
+        let mut queue = Vec::<NodeId>::new();
+        queue.push(start);
+        while let Some(n) = queue.pop() {
+            if union.find(n) == n {
+                union.union(start, n);
                 queue.extend(self.outgoing_edges_for(n)
                                  .iter()
                                  .map(|e| e.get_dest_id()));
+            }
+#[derive(Debug)]
+struct UnionFind {
+    parent: Vec<NodeId>,
+}
+
+impl UnionFind {
+    pub fn new(size: usize) -> UnionFind {
+        let parent = (0..size).collect();
+        UnionFind { parent: parent }
+    }
+
+    pub fn find(&mut self, id: NodeId) -> NodeId {
+        let mut visited_ids = vec![id];
+        let mut cur_id = id;
+        let mut par_id = self.parent[id];
+        while cur_id != par_id {
+            cur_id = par_id;
+            visited_ids.push(cur_id);
+            par_id = self.parent[cur_id];
+        }
+        for id in visited_ids {
+            self.parent[id] = par_id;
+        }
+        par_id
+    }
+
+    pub fn union(&mut self, r: NodeId, s: NodeId) {
+        let r_par = self.find(r);
+        let s_par = self.find(s);
+        if r_par != s_par {
+            self.parent[s_par] = r_par;
+        }
+    }
+
+    fn count(&self) -> usize {
+        let mut result = 0;
+        for (index, &par) in self.parent.iter().enumerate() {
+            if index == par {
+                result += 1;
             }
         }
         result
